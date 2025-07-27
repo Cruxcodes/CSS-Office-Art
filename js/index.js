5ad1e6b93300ebfbf2000e8c378177cd7821a4f6
Crux_codes <olam4461@gmail.com>
2025-07-27 15:58:58 +0100

    Enhance styling and animations in styles.scss
    - Updated background color to a variable for better maintainability.
    - Adjusted body overflow to prevent horizontal scrolling.
    - Added new positioning and scaling for elements within the body.
    - Introduced a coffee cup element with steam animation for visual effect.
    - Refined person element scaling for responsive design.
    - Modified background colors for various elements for improved aesthetics.
    - Added button styles with hover effects for better user interaction.
    - Implemented move-in and move-out animations for laptop and coffee elements.
    - Enhanced mouth animation for character interaction.
    - Added fade-out animation keyframes for smoother transitions.

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const goHomeBtn = document.getElementById("go-home");
+const personLaptop = document.querySelector(".person__laptop");
+const coffee = document.querySelector(".coffee");
+const mouth = document.querySelector(".mouth");
+const buttonInfo = document.querySelector(".button h3");
+
+const audio = new Audio("../assets/home.mp3");
+audio.loop = true;
+
+goHomeBtn.addEventListener("click", () => {
+  if (goHomeBtn.textContent === "Let me go home") {
+    // Move out and play song
+    if (personLaptop && coffee && mouth) {
+      personLaptop.classList.add("move-out-laptop");
+      personLaptop.classList.remove("move-in-laptop");
+
+      coffee.classList.add("move-out-coffee");
+      coffee.classList.remove("move-in-coffee");
+
+      mouth.classList.add("sing-mouth");
+      //   mouth.classList.remove("move-in-mouth");
+    }
+    audio.play();
+    goHomeBtn.textContent = "Go to work";
+    buttonInfo.textContent = "Yay no more work but I'm still here";
+  } else {
+    // Move in and stop song
+    if (personLaptop) {
+      personLaptop.classList.remove("move-out-laptop");
+      personLaptop.classList.add("move-in-laptop");
+
+      coffee.classList.remove("move-out-coffee");
+      coffee.classList.add("move-in-coffee");
+
+      mouth.classList.remove("sing-mouth");
+    }
+    audio.pause();
+    audio.currentTime = 0;
+    goHomeBtn.textContent = "Let me go home";
+    buttonInfo.textContent = "Sometimes life if just work and worke";
+  }
+});
